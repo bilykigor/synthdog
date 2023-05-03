4d72312472d423ef51743f337936af0f05131b48
Ihor Bilyk <i.bilyk@quadient.com>
2023-05-03 14:30:49 +0300

    force random choice

--- synthdog/elements/paper.py
+++ synthdog/elements/paper.py
@@ -3,6 +3,8 @@ Donut
 Copyright (c) 2022-present NAVER Corp.
 MIT License
 """
+import os
+import numpy as np
 from synthtiger import components, layers
 from .utils import  read_annotations, clean_texture, clean_texture2
 
@@ -40,10 +42,16 @@ class CheckPaper:
     
 class RemittancePaper:
     def __init__(self, config):
+        image_config = config.get("image", {})
+        self.image_path = image_config['paths'][0]
+        image_files = os.listdir(self.image_path)
+        self.image_files = [x for x in image_files if x.split('.')[-1]!='json']
+        
         self.image = components.BaseTexture(**config.get("image", {}))
 
     def generate(self, size=None):
-        meta = self.image.sample(size)
+        image_path = os.path.join(self.image_path,np.random.choice(self.image_files))
+        meta = self.image.sample({'path':image_path})
         texture = self.image.data(meta)
         
         size = (meta["w"],meta['h'])
